93ac906ae04899cea3ebbe92c7b9004e8ca420f4
Bill Wilke <billwilke2@gmail.com>
2020-05-12 20:35:13 -0600

    Fixes user error sad path

--- src/User.js
+++ src/User.js
@@ -1,12 +1,14 @@
 class User {
-  constructor(userData) {
-    this.id = userData.id;
-    this.name = userData.name;
-    this.email = userData.email;
-    this.strideLength = userData.strideLength
-    this.dailyStepGoal = userData.dailyStepGoal
-    this.friends = userData.friends
-  }
+	constructor(userData) {
+		if (userData) {
+			this.id = userData.id,
+			this.name = userData.name,
+			this.address = userData.address,
+			this.email = userData.email,
+			this.strideLength = userData.strideLength,
+			this.dailyStepGoal = userData.dailyStepGoal,
+			this.friends = userData.friends
+}}
   
   returnFirstName() {
     let name = this.name.split(' ');
