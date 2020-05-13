6a172d6e32bced28dc2c593c44838f05a3c4b577
Alex Eickelman <aeickelman40@gmail.com>
2020-05-12 19:12:13 -0600

    Add to userRepo test and class

--- data/test-data.js
+++ data/test-data.js
@@ -12,11 +12,51 @@ const testData = [
       8
     ]
   },
+  {
+    "id": 2,
+    "name": "Jarvis Considine",
+    "address": "30086 Kathryn Port, Ciceroland NE 07273",
+    "email": "Dimitri.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Herminia Witting",
+    "address": "85823 Bosco Fork, East Oscarstad MI 85126-5660",
+    "email": "Elwin.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
 
   {
     "userID": 1,
     "date": "2019/06/15",
     "numOunces": 37
+  },
+
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numOunces": 75
+  },
+
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numOunces": 47
   },
 
   {
@@ -27,10 +67,44 @@ const testData = [
   },
 
   {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+
+  {
     "userID": 1,
     "date": "2019/06/15",
     "numSteps": 3577,
     "minutesActive": 140,
     "flightsOfStairs": 16
-  }
+  }, 
+
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  },
 ]
+
+if (typeof module !== 'undefined') {
+    module.exports = testData;
+  }
--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,6 +1,18 @@
 class UserRepository {
-  constructor() {
-        
+  constructor(data) {   
+    this.data = data;     
+  }
+
+  getDataById(userId) {
+    return this.data.find(element => element.id === userId);
+  }
+  
+  averageUserStepGoal() {
+    let average = this.data.reduce((acc, user) => {
+      acc += user.dailyStepGoal;
+      return acc
+    }, 0) / this.data.length;
+    return Math.round(average)
   }
 }
 
