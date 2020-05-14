21f57b50272e66658b8143207ac19f75e730e0e6
Bill Wilke <billwilke2@gmail.com>
2020-05-14 14:32:29 -0600

    Working getHydrationDataById, allTimeHydration, fluidConsumedForDay

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,6 +1,33 @@
 class Hydration {
-  constructor() {
-        
+  constructor(data) {
+    if (data) {
+    this.hydrationData = data
+    }
+  }
+
+  getHydrationDataById(userId) {
+    return this.hydrationData.filter(hydrationInfo => hydrationInfo.userID === userId);
+  }
+  
+  allTimeHydration(userId) {
+    let userHydrationData = this.getHydrationDataById(userId);
+    let allTimeHydration = userHydrationData.reduce((acc, hydrationInfo) => {
+      acc += hydrationInfo.numOunces
+      return acc
+    }, 0);
+    console.log(allTimeHydration)
+   return allTimeHydration
+  }
+
+  fluidConsumedForDay(date) {
+    if(date) {
+      let dayHydration = this.hydrationData.find(element => element.date === date);
+      return dayHydration.numOunces
+    } 
+  } 
+
+  fluidConsumedForAWeek() {
+    
   }
 }
 
