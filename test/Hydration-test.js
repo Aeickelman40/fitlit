5a7f8640f5a4b85d089ad92be38ca8e3b5ea9ae6
Bill Wilke <billwilke2@gmail.com>
2020-05-14 17:24:55 -0600

    Makes fluidConsumedForWeek test

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -146,7 +146,7 @@ describe('Hydration', function() {
   // })
 
   it('should be able to get users all time hydration data', function () {
-    let hydrationData = [
+    let hydrationData = [ 
       {
         "userID": 1,
         "date": "2019/06/15",
@@ -185,6 +185,150 @@ describe('Hydration', function() {
 
       let hydration = new Hydration(hydrationData)
       expect(hydration.allTimeHydration(1)).to.equal(194)
+  })
+
+  it('should return the previous 7 days of num ounces drank', function() {
+   
+   let hydrationData = [
+    {
+      "userID": 1,
+      "date": "2019/06/14",
+      "numOunces": 82
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/14",
+      "numOunces": 27
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/14",
+      "numOunces": 43
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/15",
+      "numOunces": 37
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numOunces": 75
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/15",
+      "numOunces": 47
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numOunces": 96
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/17",
+      "numOunces": 96
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/17",
+      "numOunces": 28
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/18",
+      "numOunces": 61
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/18",
+      "numOunces": 70
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/18",
+      "numOunces": 40
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/19",
+      "numOunces": 91
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/19",
+      "numOunces": 76
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/19",
+      "numOunces": 85
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/20",
+      "numOunces": 50
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/20",
+      "numOunces": 71
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/20",
+      "numOunces": 51
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/21",
+      "numOunces": 50
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/21",
+      "numOunces": 27
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/21",
+      "numOunces": 41
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/22",
+      "numOunces": 43
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/22",
+      "numOunces": 58
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/22",
+      "numOunces": 78
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/23",
+      "numOunces": 39
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/23",
+      "numOunces": 44
+    },
+    {
+      "userID": 3,
+      "date": "2019/06/23",
+      "numOunces": 35
+    }
+  ];
+
+    let hydration = new Hydration(hydrationData)
+    expect(hydration.fluidConsumedForAWeek("2019/06/23", 1)).to.deep.equal([39, 43, 50, 50, 91, 61, 96])
   })
 
 });
