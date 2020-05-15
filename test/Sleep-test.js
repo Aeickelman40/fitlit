4b3ea8ed129c20c64a69c943f9784cca8c4e9eaf
Bill Wilke <billwilke2@gmail.com>
2020-05-15 13:29:47 -0600

    Adds test for fluidConsumedForAWeek method

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -256,5 +256,67 @@ describe('Sleep', function() {
     let sleep = new Sleep(sleepData)
     expect(sleep).to.be.an.instanceof(Sleep);
     expect(sleep.sleepQualityOnDay("2019/06/15", 3)).to.deep.equal(4.7)
+  });
+  
+  it('should be able to return a weeks worth of hours slept', function() {
+    let sleepData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "hoursSlept": 6.1,
+        "sleepQuality": 2.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "hoursSlept": 7.3,
+        "sleepQuality": 4.7
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "hoursSlept": 10.2,
+        "sleepQuality": 4.7
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "hoursSlept": 6.4,
+        "sleepQuality": 2.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "hoursSlept": 7.3,
+        "sleepQuality": 4.7
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "hoursSlept": 10.3,
+        "sleepQuality": 4.7
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "hoursSlept": 3.2,
+        "sleepQuality": 2.2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/22",
+        "hoursSlept": 5.4,
+        "sleepQuality": 4.7
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/23",
+        "hoursSlept": 10.8,
+        "sleepQuality": 4.7
+      }]
+
+    let sleep = new Sleep(sleepData)
+    expect(sleep).to.be.an.instanceof(Sleep);
+    expect(sleep.hoursSleptEachDayInAWeek("2019/06/12", 1)).to.deep.equal([ 7.3, 10.2, 6.4, 7.3, 10.3, 3.2, 5.4])
   }); 
 });
